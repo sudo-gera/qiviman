409ac13d41d7016f258b98eacf56c72a3589d110
sudo-gera <gera.tatarinov.999@mail.com>
2020-04-20 04:32:24 +0300

    fetch

--- server.py
+++ server.py
@@ -107,7 +107,7 @@ def send(text,id):
 pth=abspath(argv[0])
 pth=pth[:-len(pth.split('/')[-1])]
 chdir(pth)
-os.popen('git pull')
+os.popen('git fetch')
 try:
  js=loads(open('../qiwiman.json').read())
 except:
